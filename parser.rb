c8a0ce4afafb1198ab5383491e12f4ff8004bcba
Paul Byrne <paulgregorybyrne@gmail.com>
2023-09-29 12:46:17 +0200

    Refactor parser

--- parser.rb
+++ parser.rb
@@ -2,33 +2,29 @@ require "json"
 require "date"
 
 class Parser
-  attr_reader :search_response, :data
+  attr_reader :data, :journey_search, :legs_data, :transport_modes
 
   def initialize(search_response)
-    @search_response = search_response
     @data = JSON.parse(search_response)["data"]
+    @journey_search = data["journeySearch"]
+    @legs_data = journey_search["legs"]
+    @transport_modes = data["transportModes"]
   end
 
   def build_segments
-    journey_search = data["journeySearch"]
-    legs_data = journey_search["legs"]
-    data["journeySearch"]["journeys"].to_a.map do |segment|
-      segment_data = segment[1]
-      first_leg_id = segment_data["legs"].first
-      last_leg_id = segment_data["legs"].last
-      first_leg = legs_data[first_leg_id]
-      last_leg = legs_data[last_leg_id]
-      first_leg_departure_location_id = first_leg["departureLocation"]
-      last_leg_arrival_location_id = last_leg["arrivalLocation"]
-      departure_location = data["locations"][first_leg_departure_location_id]
-      arrival_location = data["locations"][last_leg_arrival_location_id]
+    data["journeySearch"]["journeys"].map do |segment|
+      build_segment(segment)
+    end
+  end
 
-      transport_modes = data["transportModes"]
+  def build_segment(segment)
+      segment_data = segment[1]
       legs = segment_data["legs"].map { |leg_id| legs_data[leg_id]}
+      departure_location = data["locations"][legs.first["departureLocation"]]
+      arrival_location = data["locations"][legs.last["arrivalLocation"]]
+
       products = legs.map {|leg| transport_modes[leg["transportMode"]]["mode"]}.uniq
-      alternative_ids = journey_search["sections"].reduce([]) { |acc, section| alternative = section[1]["alternatives"]; acc.concat(alternative) unless alternative.empty?; acc}
-      fare_ids = journey_search["alternatives"].map { |alternative| alternative[1]["fares"]}.flatten.uniq
-      fares = fare_ids.map { |fare_id| journey_search["fares"][fare_id] }
+      fares = build_fare_data
       {
         departure_station: departure_location["name"],
         departure_at: DateTime.parse(segment[1]["departAt"]),
@@ -37,15 +33,23 @@ class Parser
         duration_in_minutes: segment_data["duration"].scan(/\d+/).join,
         changeovers: segment_data["legs"].count - 1,
         products: products,
-        fares: fares.map { |fare| build_fare(fare) }
+        fares: fares.map { |fare| build_segment_fare(fare) }
 
       }
+  end
+
+  def build_fare_data
+    alternative_ids = journey_search["sections"].reduce([]) do |acc, section|
+      alternative = section[1]["alternatives"]; acc.concat(alternative) unless alternative.empty?; acc
     end
+
+    fare_ids = journey_search["alternatives"].map { |alternative| alternative[1]["fares"]}.flatten.uniq
+    fares = fare_ids.map { |fare_id| journey_search["fares"][fare_id] }
   end
 
   private
 
-  def build_fare(fare_data)
+  def build_segment_fare(fare_data)
     fare_type = data["fareTypes"][fare_data["fareType"]]
     {
       name: fare_type["name"],
@@ -54,5 +58,4 @@ class Parser
       comfort_class: fare_data["fareLegs"].find { |leg| leg["travelClass"] != nil}["travelClass"]["name"]
     }
   end
-
 end
